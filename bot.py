fe55d0627730be3f21fa94df653f3ed0cc350a35
Алина Хунгуреева <alinahungureeva@alinas-air.home>
2025-03-20 15:35:34 -0400

    Secure token and fix webhook

--- bot.py
+++ bot.py
@@ -513,4 +513,4 @@ def continue_workout(message, location):
 
 if __name__ == '__main__':
     set_webhook()
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
